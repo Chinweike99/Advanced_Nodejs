6b5ac1ea02a341deefc465370e4b822c5cbdda83
Chinweike99 <innocentakwolu1@gmail.com>
2024-07-04 16:07:38 +0100

    rendering ejs files

--- JSON_WEB_TOKEN/NodejsPassportLogin/server.js
+++ JSON_WEB_TOKEN/NodejsPassportLogin/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.set('view-engine', 'ejs'); // This tells our server that w are working with ejs
 const port = 3400;
 
+app.get('/', (req, res) => {
+    res.render('index.ejs', {name: "Akwolu Innocent Chinweike"})
+});
 
+app.get('/login', (req, res) => {
+    res.render("login.ejs");
+})
+
+app.get('/register', (req, res) => {
+    res.render("register.ejs");
+})
 
 app. listen(port, ()=>{
     console.log(`Listening to PORT ${port}`)
