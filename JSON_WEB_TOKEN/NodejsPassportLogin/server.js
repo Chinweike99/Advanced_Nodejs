8231a7c4ecb60a73d8298abe6c3340c01fc5f485
Chinweike99 <innocentakwolu1@gmail.com>
2024-07-06 08:35:22 +0100

    authenticate user

--- JSON_WEB_TOKEN/NodejsPassportLogin/server.js
+++ JSON_WEB_TOKEN/NodejsPassportLogin/server.js
@@ -9,13 +9,14 @@ const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
 
+const users = [];
+
 const initializePassport = require('./passportConfig')
 initializePassport(
     passport, 
     email=> users.find(user => user.email === email),
     id => users.find(user => user.id === id),
 );
-
 
 app.set('view-engine', 'ejs'); // This tells our server that w are working with ejs
 app.use(flash());
@@ -26,38 +27,36 @@ app.use(session({
 }))
 app.use(passport.initialize())
 app.use(passport.session())
-
-
 app.use(express.urlencoded({extended: true})) // Since data would be gotten from form
-const users = [];
 
 const port = 3400;
-app.get('/', (req, res) => {
-    res.render('index.ejs', {name: "Akwolu Innocent Chinweike"})
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', {name: req.user.name })
 });
 
-app.get('/login', (req, res) => {
+app.get('/login', checkNotAuthenticated, (req, res) => {
     res.render("login.ejs");
 })
 
-app.post('/login', passport.authenticate('local', {
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
     successRedirect: '/',
     failureFlash: true,
     failureRedirect: '/login'
 }))
 
-app.get('/register', (req, res) => {
+app.get('/register',checkNotAuthenticated, (req, res) => {
     res.render("register.ejs");
 })
 
-app.post('/register', async (req, res) => {
+app.post('/register', checkNotAuthenticated, async (req, res) => {
     try{
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         users.push({
             id: Date.now().toString(),
             name: req.body.name,
             email: req.body.email,
-            password: req.body.hashedPassword
+            password: hashedPassword
+            // password: req.body.hashedPassword
         })
         res.redirect('/login');
         // console.log(users.push);
@@ -67,7 +66,19 @@ app.post('/register', async (req, res) => {
     console.log(users);
 })
 
-
+// FUNCTION TO PROTECT ROUTES
+function checkAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login')
+}
+function checkNotAuthenticated (req, res, next){
+    if(req.isAuthenticated()){
+        return res.redirect('/')
+    }
+    next();
+}
 
 
 app. listen(port, ()=>{
