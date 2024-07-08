a88af5a8be2847465f6896b5cb4d07315d10a761
Chinweike99 <innocentakwolu1@gmail.com>
2024-07-08 08:57:07 +0100

    Access Token

--- JWT/server.js
+++ JWT/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const app = express();
@@ -20,19 +22,32 @@ const posts = [
 ]
 
 
-app.get('/', (req, res) =>{
-    res.render('index.ejs', { post: posts})
-})
-app.get('/posts', (req, res) =>{
-    res.json(posts)
+app.get('/posts', authenticateToken, (req, res) =>{
+    res.json(posts.filter(post => post.username === req.user.name))
     console.log(posts);
 })
 
 //Route to Authenticate user
-app.get('/login', (req, res) => {
-    // Authenticate User (Use what was learnt on paport, bcrypt to achieve this).
+app.post('/login', (req, res) => {
+    // Authenticate User (Use what was learnt on passport, bcrypt etc to achieve this).
+    const username = req.body.username
+    const user = { name: username }
 
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({ accessToken: accessToken })
 })
+
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return res.sendStatus(401)
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) =>{
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
