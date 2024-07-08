032d0f3f1d56df693775a2c8aaa12d18b98b66a1
Chinweike99 <innocentakwolu1@gmail.com>
2024-07-08 19:31:05 +0100

    updated

--- JWT/authServer.js
+++ JWT/authServer.js
@@ -6,25 +6,24 @@ const app = express();
 
 
 app.use(express.json()) // This allows our application to use json from the body up to the part that get passed to it from a request.
-const port = 3200;
+const port = 3300;
 
-const posts = [
-    {
-        username: "Chinwe",
-        email: "Chinweike@gmail.com",
-        age: 26
-    },
-    {
-        username: "Innocent",
-        email: "Chinweike@gmail.com",
-        age: 27
-    }
-]
+let refreshTokens = [];
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token
+    if(refreshToken == null) return res.sendStatus(401)
+    if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccess({ name: user.name })
+        res.json({ accessToken: accessToken })
+    })
+})
 
-
-app.get('/posts', authenticateToken, (req, res) =>{
-    res.json(posts.filter(post => post.username === req.user.name))
-    console.log(posts);
+// DELETE TOKEN
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
 })
 
 //Route to Authenticate user
@@ -33,20 +32,14 @@ app.post('/login', (req, res) => {
     const username = req.body.username
     const user = { name: username }
 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken: accessToken })
+    const accessToken = generateAccess(user)
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken)
+    res.json({ accessToken: accessToken, refreshToken: refreshToken })
 })
 
-function authenticateToken(req, res, next){
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-
-    if (token == null) return res.sendStatus(401)
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) =>{
-        if(err) return res.sendStatus(403)
-        req.user = user
-        next()
-    })
+function generateAccess(user){
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '60s'})
 }
 
 app.listen(port, () => {
--- JWT/server.js
+++ JWT/server.js
@@ -27,15 +27,6 @@ app.get('/posts', authenticateToken, (req, res) =>{
     console.log(posts);
 })
 
-//Route to Authenticate user
-app.post('/login', (req, res) => {
-    // Authenticate User (Use what was learnt on passport, bcrypt etc to achieve this).
-    const username = req.body.username
-    const user = { name: username }
-
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken: accessToken })
-})
 
 function authenticateToken(req, res, next){
     const authHeader = req.headers['authorization']
