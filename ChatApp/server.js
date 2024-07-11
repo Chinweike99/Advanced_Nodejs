55adaecb5dbca80156a5876e1fd6635f5e22ea1f
Chinweike99 <innocentakwolu1@gmail.com>
2024-07-11 23:24:30 +0100

    Broadcast and emit

--- ChatApp/server.js
+++ ChatApp/server.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const http = require('http');
-const socket = require('socket.io')
+const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app);
+const io = socketio(server);
 
 const PORT = 3000;
 
 // Get static file
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send("HELLO")
+// Run when a client is conected
+io.on('connection', (socket) =>{
+    console.log("New connection");
+
+    socket.emit('message', "Welcome to ChatApp");
+
+    // BroadCast when a user connects
+    socket.broadcast.emit();// this emits to everyone except the user that is connecting
 })
+
+// app.get('/', (req, res) => {
+//     res.send("HELLO")
+// })
 
 server.listen(PORT, ()=> {
     console.log(`Listening to port ${PORT}`)
